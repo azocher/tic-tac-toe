07b3c11186fdf9584206b6cc6eb968f4a4b1cd59
zbarovsky <zachary.barovsky@gmail.com>
2020-05-30 16:33:17 -0700

    finished some functions, displays whose next, can switch between x and o

--- js/app.js
+++ js/app.js
@@ -1,1 +1,49 @@
-console.log('Hello frontend');
+/* ------------------ DOM REFS ---------------- */
+let gameBoard = document.getElementById("gameBoard");
+let resetBtn = document.getElementById("reset");
+let showTurn = document.getElementById("displayTurn")
+
+/* ----------------- GAME Logic Variables --------------- */
+// whose turn is it
+    // let playerTurn = [];
+let turnNum = 0;
+let displayTurn = 0;
+let playerX = [];
+let playerO = [];
+// box state (clicked or unclicked)
+// gameOver state - declare win/lose/tie
+    // win arr = []
+let winCombo = [
+    ["topL", "topM", "topR"],
+    ["midL", "midM", "midR"],
+    ["botL", "botM", "botR"],
+    ["topL", "midM", "botR"],
+    ["topR", "midM", "botL"]
+];
+// winning combinations
+
+/* ------------------ Event Listeners ----------------- */
+gameBoard.addEventListener("click", boxClick);
+resetBtn.addEventListener("click", reset);
+
+
+/* ---------------- FUNCTIONS MAH DUDE ------------------- */
+
+function boxClick(e) {
+    console.log(e.target)
+    let boxSpot = e.target;
+    turnNum++
+    // plays x or o
+    if (turnNum % 2 !== 0) {
+        boxSpot.classList.add("turnX");
+        showTurn.innerText = "O is going next"
+        console.log("test for false turnNum condition")
+    } else if (turnNum % 2 == 0) {
+        boxSpot.classList.add("turnO");
+        showTurn.innerText = "X is going next"
+    }
+
+}
+
+
+
