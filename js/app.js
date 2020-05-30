5e446ac97a2e1e4789cded5eb58a5c94678c946f
Eliott R Davidson <fussypuppet@Eliotts-MacBook-Pro.local>
2020-05-30 12:55:11 -0700

    Added basic AI that looks 1 turn ahead.  It works, but it also likes to take your turn for you afterward.  Still, it works well enough to justify a commit

--- js/app.js
+++ js/app.js
@@ -2,147 +2,102 @@ document.addEventListener('DOMContentLoaded', function(){
     console.log("in js file!");
 
     // get DOM refs
-   const gameSpace = document.querySelector(".gameSpace");
-   const resetButton = document.getElementById("reset");
-   const undoButton = document.getElementById("undo");
-   const messageBox = document.getElementById("message");
-   const nameInputs = {};
-   nameInputs.X = document.getElementById("nameX");
-   nameInputs.O = document.getElementById("nameO");
-   const computerPlayerCheckboxes = {};
-   computerPlayerCheckboxes.X = document.getElementById("computerX");
-   computerPlayerCheckboxes.O = document.getElementById("computerO");
-   const smartAI = document.getElementById("smartAI");
-   const winTable = document.querySelector(".winsOverTime");
+    const gameSpace = document.querySelector(".gameSpace");
+    const resetButton = document.getElementById("reset");
+    const undoButton = document.getElementById("undo");
+    const messageBox = document.getElementById("message");
+    const nameInputs = {};
+    nameInputs.X = document.getElementById("nameX");
+    nameInputs.O = document.getElementById("nameO");
+    const computerPlayerCheckboxes = {};
+    computerPlayerCheckboxes.X = document.getElementById("computerX");
+    computerPlayerCheckboxes.O = document.getElementById("computerO");
+    const smartAIBox = document.getElementById("smartAI");
+    const winTable = document.querySelector(".winsOverTime");
 
-   // initialize game globals
-   const WIN_CONDITIONS_MASTER = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0 ,4, 8], [6, 4, 2]];
-   let remainingWinConditions = [];
-   let activeGame = false;
-   let currentTurn = ""; // marks which player's turn it is
-   let turnCount = null; // marks the number of the current turn
-   let moveHistory = [];
-   let playerNames = {
-       X: "",
-       O: ""
-   }
-
-
-   // event listeners
-   resetButton.addEventListener("click", reset);
-   undoButton.addEventListener("click", undo);
-   let squareCount = gameSpace.children.length;
-   for (let i = 0; i < squareCount; i++){
-       let thisSquare = gameSpace.children[i];
-       thisSquare.addEventListener("click", squareClick);
-       thisSquare.addEventListener("mouseover", squareHover);
-       thisSquare.addEventListener("mouseleave", squareHoverNoMore);
-   }
-   computerPlayerCheckboxes.X.addEventListener("click", toggleComputerPlayer);
-   computerPlayerCheckboxes.O.addEventListener("click", toggleComputerPlayer);
-
-   // if the user checks a computer-player checkbox, give that player the name of "Computer"
-   function toggleComputerPlayer(e){
-       console.log(e);
-       let relevantTextBox = nameInputs[e.target.value];
-       if (e.target.checked){
-           relevantTextBox.value = "Computer"
-           relevantTextBox.disabled = true;
-       } else {
-           relevantTextBox.value = "";
-           relevantTextBox.disabled = false;
-       }
-   }
-
-   function init(){
-    console.log("in init");
-    //repopulate win conditions.  Lines that receive a mix of Xs and Os as the game gets played will be spliced from this copy
-    remainingWinConditions = [];
-    for (condition of WIN_CONDITIONS_MASTER){
-        remainingWinConditions.push(condition);
+    // initialize game globals
+    const WIN_CONDITIONS_MASTER = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0 ,4, 8], [6, 4, 2]];
+    let remainingWinConditions = [];
+    let activeGame = false;
+    let currentTurn = ""; // marks which player's turn it is
+    let turnCount = null; // marks the number of the current turn
+    let moveHistory = [];
+    let playerNames = {
+        X: "",
+        O: ""
     }
-    //clear all board values (for game reset)
-    for (square of gameSpace.children){
-        square.children[0].innerHTML = "";
-        square.classList.remove("red");
+    let smartAI = "false";
+
+
+    // event listeners
+    resetButton.addEventListener("click", reset);
+    undoButton.addEventListener("click", undo);
+    let squareCount = gameSpace.children.length;
+    for (let i = 0; i < squareCount; i++){
+        let thisSquare = gameSpace.children[i];
+        thisSquare.addEventListener("click", squareClick);
+        thisSquare.addEventListener("mouseover", squareHover);
+        thisSquare.addEventListener("mouseleave", squareHoverNoMore);
     }
-    playerNames.X = (nameInputs.X.value || "Player X");
-    nameInputs.X.disabled = true;
-    playerNames.O = (nameInputs.O.value || "Player O");
-    nameInputs.O.disabled = true;
-    messageBox.innerHTML = `Current turn: ${playerNames.X}`
-    currentTurn = "X";
-    moveHistory = [];
-    turnCount = 1;
-    activeGame = true;
-    if (playerNames.X === "Computer"){
-        setTimeout(takeComputerTurn, 750);
-    }
-}
+    computerPlayerCheckboxes.X.addEventListener("click", toggleComputerPlayer);
+    computerPlayerCheckboxes.O.addEventListener("click", toggleComputerPlayer);
 
-   // stub out event listener functions
-   function reset() {
-       console.log("Reset button clicked!");
-       init();
-   }
-
-   function undo() {
-       console.log("Undo button clicked!");
-   }
-
-   function squareClick(e, computerChoice) {
-       // verify that a game is active, that the player hasn't clicked the margin between squares, and that the square is empty
-       if (activeGame){
-           if (e.target.id && !e.target.children[0].innerHTML){ 
-               console.log(`Square ${e.target.id} has been clicked!`);
-               squareHoverNoMore(e);
-               e.target.children[0].innerHTML = currentTurn;
-               advanceTurn();
-               checkForWin();
-               pruneWinConditions();
-           }
-       }
-    }
-
-    function squareHover(e){
-        // make an X or O appear in a cell before click happens, just for fun and as a reminder of whose turn it is
-        console.log("in squareHover");
-        if (activeGame){
-            if (e.target.id && !e.target.children[0].innerHTML){
-                e.target.classList.add("hover");
-            }
+   
+    // if the user checks a computer-player checkbox, give that player the name of "Computer"
+    function toggleComputerPlayer(e){
+        console.log(e);
+        let relevantTextBox = nameInputs[e.target.value];
+        if (e.target.checked){
+            relevantTextBox.value = "Computer"
+            relevantTextBox.disabled = true;
+        } else {
+            relevantTextBox.value = "";
+            relevantTextBox.disabled = false;
         }
     }
 
-    function squareHoverNoMore(e){
-        if (activeGame){
-            if (e.target.id && !e.target.children[0].innerHTML){
-                e.target.classList.remove("hover");
-            }
+   
+    /*****MAIN GAMEPLAY FUNCTIONS ***/
+
+    function init(){
+        console.log("in init");
+        //repopulate win conditions.  Lines that receive a mix of Xs and Os as the game gets played will be spliced from this copy
+        remainingWinConditions = [];
+        for (condition of WIN_CONDITIONS_MASTER){
+            remainingWinConditions.push(condition);
+        }
+        //clear all board values (for game reset)
+        for (square of gameSpace.children){
+            square.children[0].innerHTML = "";
+            square.classList.remove("red");
+        }
+        playerNames.X = (nameInputs.X.value || "Player X");
+        nameInputs.X.disabled = true;
+        playerNames.O = (nameInputs.O.value || "Player O");
+        nameInputs.O.disabled = true;
+        messageBox.innerHTML = `Current turn: ${playerNames.X}`
+        currentTurn = "X";
+        moveHistory = [];
+        turnCount = 1;
+        activeGame = true;
+        if (playerNames.X === "Computer"){
+            setTimeout(takeComputerTurn, 750);
+        }
+        if (smartAIBox.checked){
+            smartAI = true;
         }
     }
 
-    function checkForWin(){
-        console.log("in CheckForWin");
-        if (turnCount >= 5){ //turn 5 is the earliest anyone can win
-            console.log("It's turn " + turnCount + ", continuing with win check");
-            for (let i = 0; i < remainingWinConditions.length; i++){ //iterate through every remaining winnable stripe
-                let thisStripe = [];
-                for (let j = 0; j < remainingWinConditions[i].length; j++){ // make a list of all the values in that stripe
-                    thisStripe.push(gameSpace.children[remainingWinConditions[i][j]].children[0].innerHTML);
-                    if (j === 2){
-                        console.log("Done with a stripe: " + JSON.stringify(thisStripe));
-                        if (thisStripe[0] === "X" || thisStripe[0] === "O"){ // if all values are the same and are not blank
-                            if ((thisStripe[0] === thisStripe[1]) && thisStripe[0] === thisStripe[2]){ 
-                                console.log("Ending game with a win for " + thisStripe[0]);
-                                for (let k = 0; k < 3; k++){ // color the winning streak
-                                    gameSpace.children[remainingWinConditions[i][k]].classList.add("red");
-                                }
-                                endGame(thisStripe[0]); // end the game, player holding this stripe wins.
-                            }
-                        }
-                    }
-                }
+    function squareClick(e) {
+        // verify that a game is active, that the player hasn't clicked the margin between squares, and that the square is empty
+        if (activeGame){
+            if (e.target.id && !e.target.children[0].innerHTML){ 
+                console.log(`Square ${e.target.id} has been clicked!`);
+                squareHoverNoMore(e);
+                e.target.children[0].innerHTML = currentTurn;
+                advanceTurn();
+                checkForWin();
+                pruneWinConditions();
             }
         }
     }
@@ -164,29 +119,64 @@ document.addEventListener('DOMContentLoaded', function(){
 
     //find which squares are available to play, and pick a random one
     function takeComputerTurn(){
-        let availableSquares = [];
-        for (thisSquare of gameSpace.children){
-            if (!thisSquare.children[0].innerHTML){  // if this square has no XO entry, add it to available squares
-                console.log(thisSquare);
-                availableSquares.push(thisSquare);
-            }
-        }
-        if (availableSquares.length > 0){
-            let pickedSquareIndex = Math.floor(Math.random()*availableSquares.length);
-            squareClick({target: availableSquares[pickedSquareIndex]});
+        switch (smartAI){
+            case true:
+                // make AI attempt to finish streaks if it has 2, and otherwise defensively block imminent opponent wins
+                let threat = false;  //will turn into a square to play if AI detects it will lose next turn.
+                let moveMade = false;
+                for (thisWinCondition of remainingWinConditions){  // but first, check for & play any move that will win right now
+                    let computerSquares = [];
+                    let humanSquares = [];
+                    let emptySquares = [];
+                    for (thisSquareID of thisWinCondition){
+                        let thisSquareValue = gameSpace.children[thisSquareID].children[0].innerHTML
+                        if (!thisSquareValue){
+                            emptySquares.push(thisSquareID);
+                        } else if (playerNames[thisSquareValue] === "Computer"){
+                            computerSquares.push(thisSquareID);
+                        } else {
+                            humanSquares.push(thisSquareID);
+                        }
+                    }
+                    if ((computerSquares.length === 2) && (emptySquares.length === 1)){
+                        squareClick({target: gameSpace.children[emptySquares[0]]});
+                        moveMade = true;
+                        break;
+                    } else if ((humanSquares.length === 2) && (emptySquares.length === 1)){
+                        threat = gameSpace.children[emptySquares[0]];
+                    }
+                }
+                if (threat && !moveMade){
+                    squareClick({target: threat});
+                } // intentionally no break!  If there is no smart action to take, go ahead and take a random naive one.
+                if (moveMade){
+                    break;
+                }
+            case false:
+                let availableSquares = [];
+                for (thisSquare of gameSpace.children){
+                    if (!thisSquare.children[0].innerHTML){  // if this square has no XO entry, add it to available squares
+                        console.log(thisSquare);
+                        availableSquares.push(thisSquare);
+                    }
+                }
+                if (availableSquares.length > 0){
+                    let pickedSquareIndex = Math.floor(Math.random()*availableSquares.length);
+                    squareClick({target: availableSquares[pickedSquareIndex]});
+                }
         }
     }
 
     //check whether each winning stripe has both Xs and Os.  If so, remove it from the list of possible win conditions.
     function pruneWinConditions(){
         //console.log("In Prune Function");
-        for (i = 0; i < remainingWinConditions.length; i++){
+        for (let i = 0; i < remainingWinConditions.length; i++){
             let hasX = false;
             let hasO = false;
             let thisRow = [];
             if (remainingWinConditions[i]){
                 //console.log(JSON.stringify(remainingWinConditions));
-                for (j = 0; j < remainingWinConditions[i].length; j++){
+                for (let j = 0; j < remainingWinConditions[i].length; j++){
                     let thisSquareValue = gameSpace.children[remainingWinConditions[i][j]].children[0].innerHTML;
                     switch (thisSquareValue){
                         case "X":
@@ -222,6 +212,31 @@ document.addEventListener('DOMContentLoaded', function(){
             endGame("tie");
         } 
         // this function does not currently look forward to see if a win is still possible, only backward.
+    }
+
+    function checkForWin(){
+        console.log("in CheckForWin");
+        if (turnCount >= 5){ //turn 5 is the earliest anyone can win
+            console.log("It's turn " + turnCount + ", continuing with win check");
+            for (let i = 0; i < remainingWinConditions.length; i++){ //iterate through every remaining winnable stripe
+                let thisStripe = [];
+                for (let j = 0; j < remainingWinConditions[i].length; j++){ // make a list of all the values in that stripe
+                    thisStripe.push(gameSpace.children[remainingWinConditions[i][j]].children[0].innerHTML);
+                    if (j === 2){
+                        console.log("Done with a stripe: " + JSON.stringify(thisStripe));
+                        if (thisStripe[0] === "X" || thisStripe[0] === "O"){ // if all values are the same and are not blank
+                            if ((thisStripe[0] === thisStripe[1]) && thisStripe[0] === thisStripe[2]){ 
+                                console.log("Ending game with a win for " + thisStripe[0]);
+                                for (let k = 0; k < 3; k++){ // color the winning streak
+                                    gameSpace.children[remainingWinConditions[i][k]].classList.add("red");
+                                }
+                                endGame(thisStripe[0]); // end the game, player holding this stripe wins.
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     function updateWinTable(result){
@@ -284,5 +299,35 @@ document.addEventListener('DOMContentLoaded', function(){
         activeGame = false;
         nameInputs.X.disabled = false;
         nameInputs.O.disabled = false;
+    }
+
+
+    /*****OTHER FUNCTIONS*****/
+
+    function squareHover(e){
+        // make an X or O appear in a cell before click happens, just for fun and as a reminder of whose turn it is
+        console.log("in squareHover");
+        if (activeGame){
+            if (e.target.id && !e.target.children[0].innerHTML){
+                e.target.classList.add("hover");
+            }
+        }
+    }
+
+    function squareHoverNoMore(e){
+        if (activeGame){
+            if (e.target.id && !e.target.children[0].innerHTML){
+                e.target.classList.remove("hover");
+            }
+        }
+    }
+    
+    function reset() {
+        console.log("Reset button clicked!");
+        init();
+    }
+
+    function undo() {
+        console.log("Undo button clicked!");
     }
 })
