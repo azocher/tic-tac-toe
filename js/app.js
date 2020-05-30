c1a614105e7a475e019fe1ecbae3fa78c998ca0a
Eliott R Davidson <fussypuppet@Eliotts-MacBook-Pro.local>
2020-05-29 18:13:59 -0700

    Squares fill with Xs and Os, and turns alternate

--- js/app.js
+++ js/app.js
@@ -10,7 +10,12 @@ document.addEventListener('DOMContentLoaded', function(){
 
    // initialize game globals
    const WIN_CONDITIONS_MASTER = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0 ,4, 8], [6, 4, 2]];
+   let remainingWinConditions = [];
    let activeGame = false;
+   let currentTurn = ""; // marks which player's turn it is
+   let turnCount = null; // marks the number of the current turn
+   let moveHistory = [];
+
 
    // event listeners
    resetButton.addEventListener("click", reset);
@@ -24,15 +29,18 @@ document.addEventListener('DOMContentLoaded', function(){
    function init(){
     console.log("in init");
     //repopulate win conditions.  Lines that receive a mix of Xs and Os as the game gets played will be spliced from this copy
-    let remaining_win_conditions = [];
+    remainingWinConditions = [];
     for (condition of WIN_CONDITIONS_MASTER){
-        remaining_win_conditions.push(condition);
+        remainingWinConditions.push(condition);
     }
     //clear all board values (for game reset)
     for (square of gameSpace.children){
         square.children[0].innerHTML = "";
     }
     messageBox.innerHTML = "Current turn: Player X"
+    currentTurn = "X";
+    moveHistory = [];
+    turnCount = 1;
     activeGame = true;
 }
 
@@ -48,8 +56,20 @@ document.addEventListener('DOMContentLoaded', function(){
 
    function squareClick(e) {
        if (activeGame && e.target.id){ // clicking on the margins between squares triggers an event that lacks a square id
-           console.log(e);
            console.log(`Square ${e.target.id} has been clicked!`);
+           e.target.children[0].innerHTML = currentTurn;
+           advanceTurn();
        }
+    }
+
+    function advanceTurn(){
+        turnCount++;
+        if (currentTurn === "X"){
+            currentTurn = "O";
+            messageBox.innerHTML = "Current turn: Player O";
+        } else {
+            currentTurn = "X";
+            messageBox.innerHTML = "Current turn: Player X";
+        }
     }
 })
