829b105c70fd677cd5883eadb8e40d80f1f1c892
Nick Hvattum <nhvattum@gmail.com>
2020-05-30 17:46:51 -0700

    Switches player and updates player box

--- js/app.js
+++ js/app.js
@@ -1,11 +1,48 @@
+/*---------------- DOM REFERENCES ------------------*/
 let playerBox = document.getElementById('playerBox');
-let cell1 = document.getElementById('cell1');
-let cell2 = document.getElementById('cell2');
-let cell3 = document.getElementById('cell3');
-let cell4 = document.getElementById('cell4');
-let cell5 = document.getElementById('cell5');
-let cell6 = document.getElementById('cell6');
-let cell7 = document.getElementById('cell7');
-let cell8 = document.getElementById('cell8');
-let cell9 = document.getElementById('cell9');
-let cells = document.getElementById('cells')
+let grid = document.querySelector('.grid')
+let turn = 1;
+let playerX = "X";
+let playerO = "O";
+/*------------------- GAME LOGIC -------------------*/
+
+
+/*---------------- EVENT LISTENERS -----------------*/
+
+grid.addEventListener('click', checkCell);
+
+
+
+/*----------------- FUNCTIONS ---------------------*/
+
+function checkCell(e) {
+    if (e.target.textContent.value == null) {
+        cellClick(e);
+        turn++;
+    }else {
+        console.log("Try again");
+    }
+}
+
+
+function cellClick(e) {
+    if (turn % 2 == 0) {
+        playerBox.textContent = "It is player X's turn";
+        e.target.textContent = playerO;
+        // turn++;
+    }else {
+        playerBox.textContent = "It is player O's turn";
+        e.target.textContent = playerX;
+        // turn++;
+    }
+}
+
+
+
+
+
+
+
+
+
+
