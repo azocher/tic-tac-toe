46447dc858698f68953de8b782e58880318892e5
Salman Malik <salmanmalik@Salmans-Air.attlocal.net>
2020-05-31 03:00:05 -0500

    began setting up DOM Refs and Logic variables, including win conditions. Currently attempting to set up Event listener functions that register moves

--- js/app.js
+++ js/app.js
@@ -1,18 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
     /*-----------> DOM REFS <-------------------*/ 
-    let gameFace = document.getElementById('gameFace');
-    let topLeftText = document.getElementById('topLeft')
-    // let button
+    let everyMove = document.querySelectorAll('.moveSpace');
+    let gameSpace = document.querySelector('.gameFace');
+
 
     /*-----------> Game Logic Variables <-----------*/ 
     let userTurn = null;
     let computerTurn = null;
+    let playerMove = "X";
+    let compMove = "O"
+    let winConditions = [[0,1,2], 
+    [3,4,5], 
+    [6,7,8], 
+    [0,4,8], 
+    [6,4,2], 
+    [0,3,6],
+    [1,4,7]
+    [2,5,8]];
+
+    let gameState = ["", "", "", "", "", "", "", "", ""]
 
     /*-----------> Event Listener <-----------*/ 
-    gameFace.addEventListener('click', user);
-    
-    function user() {
-        var myMove = document.getElementById('TopLeftText');
-        myMove.innerHTML = "X";
+    function init() {
+        for (var i = 0; i < everyMove.length; i++) {
+            everyMove[i].innerText = '';
+            everyMove[i].addEventListener('click', click)
+        }
     }
+
+    function click(e) {
+        console.log(e.target.id);
+    }
+
 })
