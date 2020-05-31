860fce6724121a0368609b6033a7be7d2f677e2d
Salman Malik <salmanmalik@Salmans-Air.attlocal.net>
2020-05-31 01:16:52 -0500

    Created gameFace grid through border CSS elements, attempting to use DOM elements to track turns and struggling. Considering updating my HTML to reflect a grid display

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('Hello frontend');
+document.addEventListener('DOMContentLoaded', function() {
+    /*-----------> DOM REFS <-------------------*/ 
+    let gameFace = document.getElementById('gameFace');
+    let topLeftText = document.getElementById('topLeft')
+    // let button
+
+    /*-----------> Game Logic Variables <-----------*/ 
+    let userTurn = null;
+    let computerTurn = null;
+
+    /*-----------> Event Listener <-----------*/ 
+    gameFace.addEventListener('click', user);
+    
+    function user() {
+        var myMove = document.getElementById('TopLeftText');
+        myMove.innerHTML = "X";
+    }
+})
