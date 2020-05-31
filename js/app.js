d45cca7948dadc5ec369ac302b83c7134c8eafa9
zbarovsky <zachary.barovsky@gmail.com>
2020-05-30 22:19:23 -0700

    almost there! Just need to add for function to compare arrays

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ let showTurn = document.getElementById("displayTurn")
 /* ----------------- GAME Logic Variables --------------- */
 let turnNum = 0;
 let displayTurn = 0;
+//let endGame = [];
+let playerX = [];
+let playerO = [];
 
 // box state (clicked or unclicked)
 // gameOver state - declare win/lose/tie
@@ -17,8 +20,7 @@ let winCombo = [
     ["topL", "midM", "botR"],
     ["topR", "midM", "botL"]
 ];
-let playerX = [];
-let playerO = [];
+
 // winning combinations
 
 /* ------------------ Event Listeners ----------------- */
@@ -28,8 +30,9 @@ resetBtn.addEventListener("click", reset);
 
 /* ---------------- FUNCTIONS MAH DUDE ------------------- */
 
+// click box, display x or o, add class to div, display whose turn is next, and log it to appropriate player array
 function boxClick(e) {
-    console.log(e.target)
+    //console.log(e.target)
     let boxSpot = e.target;
     // plays x or o
     if (boxSpot.classList.contains("turnX") || boxSpot.classList.contains("turnO")) {
@@ -38,15 +41,49 @@ function boxClick(e) {
         if (turnNum % 2 !== 0) {
             boxSpot.classList.add("turnX");
             showTurn.innerText = "O is going next"
-            //console.log("test for false turnNum condition")
+            let choice = boxSpot.id;
+            if (playerX.indexOf(choice) < 0) {
+                playerX.push(choice)
+                console.log(playerX)
+            }
         }; 
         if (turnNum % 2 == 0) {
             boxSpot.classList.add("turnO");
             showTurn.innerText = "X is going next"
+            let choice = boxSpot.id;
+            if (playerO.indexOf(choice) < 0) {
+                playerO.push(choice)
+                console.log(playerO);
+            }
         };
         turnNum++
+        
     }  
+    endGame();
+};
+
+// function for check for game winning results
+
+function endGame(playerX, playerO, winCombo) {
+    if (playerX.includes("topL", "topM", "topR")) {
+        console.log("player X wins")
+    }
 }
+// if in both playerX and winCombo then add to new array. If new array = combo then declare winner
+// if else to check for all classes attached to a div 
+
+// for (length of winCombo, var i) { 
+//     // create and set matchCombo = 0
+//   ​
+//     for (length of current i/nestedArr, var j) {
+//       // check if playerX or playerO array.includes(nestedArr[j])
+//       // if true, increase matchCombo++ 
+//       // else, do nothing
+//     }
+//   ​
+//   } 
+
+
 
 
 
